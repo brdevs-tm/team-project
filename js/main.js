926062c927ba66136184b5174c85dec3ff898022
khamidov <bright19.off@gmail.com>
2023-04-25 11:34:27 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function toggleText() {
+//   var textElement = document.getElementById("myText");
+//   if (textElement.style.display === "none") {
+//     textElement.style.display = "block";
+//   } else {
+//     textElement.style.display = "none";
+//   }
+// }
